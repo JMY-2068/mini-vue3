5d7027ad0fc4cc5f371f113d4a5e11c4e838fb48
JMY-2068 <1404846021@qq.com>
2022-02-22 20:47:08 +0800

    实现 effect 的 stop 功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,38 +1,70 @@
+import { extend } from '../shared'
+
 class ReactiveEffect {
     private _fn: any
+    deps = []
+    active = true
+    onStop?: () => void
     constructor(fn, public scheduler?) {
         this._fn = fn
+        this.scheduler = scheduler
     }
     run() {
         activeEffect = this
+        console.log('执行run方法')
         return this._fn()
     }
+    stop() {
+        if (this.active) {
+            cleanUpEffect(this)
+            if (this.onStop) {
+                this.onStop()
+            }
+            this.active = false
+        }
+
+    }
+}
+
+function cleanUpEffect(effect) {
+    effect.deps.forEach((dep: any) => {
+        dep.delete(effect)
+    })
 }
 
 const targetMap = new Map()
 // 收集依赖方法
 export function track(target, key) {
+    console.log('触发收集依赖')
     // target -> key -> dep
     let depsMap = targetMap.get(target)
     if (!depsMap) {
+        console.log('初始化targetMap')
         depsMap = new Map()
         targetMap.set(target, depsMap)
     }
 
     let dep = depsMap.get(key)
     if (!dep) {
+        console.log('初始化dep')
         dep = new Set()
         depsMap.set(key, dep)
     }
+
+    if (!activeEffect) return
+    console.log('将activeEffect添加到dep中')
     dep.add(activeEffect)
+    activeEffect.deps.push(dep)
 }
 
 // 触发依赖方法
 export function trigger(target, key) {
+    console.log('进入触发依赖')
     let depsMap = targetMap.get(target)
     let dep = depsMap.get(key)
     for (const effect of dep) {
         if (effect.scheduler) {
+            console.log('触发依赖, 并且执行scheduler')
             effect.scheduler()
         } else {
             effect.run()
@@ -42,8 +74,18 @@ export function trigger(target, key) {
 
 let activeEffect
 export function effect(fn, options: any = {}) {
-    const { scheduler } = options
-    const _effect = new ReactiveEffect(fn, scheduler)
+    console.log('初始化ReactiveEffect')
+    const _effect = new ReactiveEffect(fn, options.scheduler)
+    extend(_effect, options)
+
     _effect.run()
-    return _effect.run.bind(_effect)
+
+    const runner: any = _effect.run.bind(_effect)
+    runner.effect = _effect
+
+    return runner
+}
+
+export function stop(runner) {
+    runner.effect.stop()
 }
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -3,11 +3,13 @@ import { track, trigger } from './effect'
 export function reactive(raw) {
     return new Proxy(raw, {
         get(target, key) {
+            console.log('触发get')
             const res = Reflect.get(target, key)
             track(target, key)
             return res
         },
         set(target, key, value) {
+            console.log('触发set')
             const res = Reflect.set(target, key, value)
             trigger(target, key)
             return res
